02cbed6b993825cabd3de216e7912d73447782df
Landers <christina@pulsd.com>
2012-03-15 21:41:07 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tipsi::Application.config.secret_token = 'c1decdf6b242a53dd4e34c40de5bcc0e486b7ac4afb5449bbadead18b06a4d4ac222d6838ea9a662d967b5cc7b0cd307b93fe81442307faa0c756ac9d8a252a7'
